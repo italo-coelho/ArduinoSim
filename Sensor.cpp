3df543c78e89527fd229cc7bb18011cc23e10cea
rafao123 <langerabelo@gmail.com>
2021-05-12 11:42:22 -0300

    getValue do sensor ta pronto

--- Sensor.cpp
+++ Sensor.cpp
@@ -1,11 +1,52 @@
 #include "Sensor.h"
+#include <random>
 
 namespace prog3
 {
     Sensor::Sensor()
     {
-        Tipo = ;
-
-
+        Tipo = TEMPERATURA;
+        Protocolo = SPI;
     }
+
+    Sensor::Sensor (int tipo)
+    {
+        Tipo = tipo;
+        Protocolo = SPI;
+    }
+
+    Sensor::Sensor (int tipo, int protocolo)
+    {
+        Tipo = tipo;
+        Protocolo = protocolo;
+    }
+
+    double Sensor::getValue() const
+    {   
+        std::default_random_engine gerador;
+
+        if(Tipo==TEMPERATURA)
+        {   
+            std::uniform_real_distribution<double> temp_range(20.0, 25.0);
+            return temp_range(gerador);
+
+        }
+        if(Tipo==PRESSAO)
+        {   
+            std::uniform_real_distribution<double> press_range(1010.0, 1050.0);
+            return press_range(gerador);
+            
+        }
+        if(Tipo==UMIDADE)
+        {   
+            std::uniform_real_distribution<double> humidity_range(50.0, 100.0);
+            return humidity_range(gerador);
+            
+        }
+
+        
+    }
+
+
+
 }
--- Sensor.h
+++ Sensor.h
@@ -3,31 +3,30 @@
 
 #include <string>
 #include <vector>
-#include "pino.h"
+
+
 
 namespace prog3
 {   
     enum protocolo {SPI = 1, I2C = 2, UART = 3};
-    enum tipos_sensor {TEMPERATURA, PRESSAO, UMIDADE};
+    enum tipos_sensor {TEMPERATURA = 1, PRESSAO = 2, UMIDADE = 3};
 
     class Sensor
     {
     private:
-        
-        
-        std::vector<pino> Pinos;
+    
         int Tipo;
         int Protocolo;
 
     public:
         Sensor();
         Sensor(int tipo);
-        Sensor(int protocolo, int tipo);
+        Sensor(int tipo, int protocolo);
 
         double getValue()const;
         int getTipo()const;
         int getProtocolo()const;
-        std::string getPinos();
+
 };
 
 }
